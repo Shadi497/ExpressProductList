29795408a10d330b1246a8d05b4bddf38d7dde2d
Shadi497 <b.shadi.h@gmail.com>
2021-02-09 09:53:10 +0300

    Express CRUD by routes & controllers

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ const productRoutes = require("./routes/products");
 App.use(express.json());
 App.use("/products", productRoutes);
 
-// db.sequelize.sync({ alter: true });
-db.sequelize.sync({ force: true });
+db.sequelize.sync({ alter: true });
+// db.sequelize.sync({ force: true });
 
 App.listen(8000, () => {
   console.log("Application is running");
